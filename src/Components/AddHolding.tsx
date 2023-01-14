6f22fcf4a4447ff0f98a8824bf20fc669a08a20d
Gregory Shoup <gregshoup@gmail.com>
2023-01-14 18:29:21 -0500

    add holding now in modal

--- src/Components/AddHolding.tsx
+++ src/Components/AddHolding.tsx
@@ -1,12 +1,27 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Button, Input } from 'antd';
+import { Button, Input, Modal } from 'antd';
 
 interface AddHoldingProps {
     loggedInUserID: number
 }
 
 function AddHolding({ loggedInUserID }: AddHoldingProps) {
+    // from ant.design documentation
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+    //end ant.design code
 
     const [stockName, setStockName] = useState('')
     const [stockTicker, setStockTicker] = useState('')
@@ -43,15 +58,20 @@ function AddHolding({ loggedInUserID }: AddHoldingProps) {
 
     return (
         <>
-            <div className='form-div'>
-                <h4>Add Holding</h4>
-                <form onSubmit={(event) => handleAddHolding(event)}>
-                    <Input type="text" placeholder="stock name" onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleStockName(event)} />
-                    <Input type="text" placeholder="stock ticker" onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleStockTicker(event)} />
-                    <Input type="number" placeholder="number of shares" onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleStockShares(event)} />
-                    <Button type='primary' htmlType='submit'>SUBMIT</Button>
-                </form>
-            </div>
+            <Button type="primary" onClick={showModal}>
+                ADD HOLDING
+            </Button>
+            <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} okText='DONE'>
+                <div className='form-div'>
+                    <h4>Add Holding</h4>
+                    <form onSubmit={(event) => handleAddHolding(event)}>
+                        <Input type="text" placeholder="stock name" onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleStockName(event)} />
+                        <Input type="text" placeholder="stock ticker" onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleStockTicker(event)} />
+                        <Input type="number" placeholder="number of shares" onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleStockShares(event)} />
+                        <Button type='primary' htmlType='submit'>SUBMIT</Button>
+                    </form>
+                </div>
+            </Modal>
         </>
     );
 }
